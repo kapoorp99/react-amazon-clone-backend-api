85ddd50a73a4310e0139631efccceb9ee2d446e1
kapoorp99 <kapoorprakhar99@gmail.com>
2021-01-29 18:32:36 +0530

    name added

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
 
-
 app = FastAPI()
-
-#domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
 
 
 @app.get("/")
