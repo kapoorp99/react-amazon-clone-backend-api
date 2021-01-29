5bc1096cb021de4c543a84c5fc12ee589206bbe7
kapoorp99 <kapoorprakhar99@gmail.com>
2021-01-29 18:35:43 +0530

    stripe api added

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+import stripe
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 
@@ -6,3 +10,25 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Hello, this is Prakhar Kapoor here"}
+
+
+@app.post("/payments/create/")
+async def process_payment(total: str):
+    totalf = int(total)
+    payment_intent = stripe.PaymentIntent.create(
+        amount=totalf,
+        currency="usd",
+        payment_method_types=["card"],
+        description="Software development services",
+        shipping=dict(
+            name="Prakhar Kapoor",
+            address=dict(
+                line1="510 Townsend St",
+                postal_code="98140",
+                city="San Francisco",
+                state="CA",
+                country="US",
+            ),
+        ))
+    json_compatible_item_data = jsonable_encoder(payment_intent.client_secret)
+    return JSONResponse(content=json_compatible_item_data)
