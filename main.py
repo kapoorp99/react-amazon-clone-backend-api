4c15d1f1bba87bd423185ef39d8425708afc27bd
kapoorp99 <kapoorprakhar99@gmail.com>
2021-01-31 18:12:22 +0530

    giving up is not in my dictionary

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from fastapi import FastAPI
-import stripe
-
 app = FastAPI()
-
-stripe.api_key = "sk_test_51ICjFIGOe2LN572AJ6Ivmu47c1tbLETmMCvi7tDXyZ5201vje9kqFcRSjyblWrjUEK792qJkNe84zeldGqgAwApH00ebOStQB6"
-
 
 @app.get('/')
 def home():
