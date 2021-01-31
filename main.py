ccd41bca4ebe35f4b92a784f35e915b229f279e6
kapoorp99 <kapoorprakhar99@gmail.com>
2021-01-31 17:58:40 +0530

    giving up is not in my dictionary

--- main.py
+++ main.py
@@ -9,25 +9,3 @@ stripe.api_key = "sk_test_51ICjFIGOe2LN572AJ6Ivmu47c1tbLETmMCvi7tDXyZ5201vje9kqF
 @app.get('/')
 def home():
     return {"message": "Hello, this is Prakhar Kapoor here"}
-
-
-@app.post('/payments/create/')
-def process_payment(total: str):
-    totalf = int(total)
-    payment_intent = stripe.PaymentIntent.create(
-        amount=totalf,
-        currency="usd",
-        payment_method_types=["card"],
-        description="Software development services",
-        shipping=dict(
-            name="Prakhar Kapoor",
-            address=dict(
-                line1="510 Townsend St",
-                postal_code="98140",
-                city="San Francisco",
-                state="CA",
-                country="US",
-            ),
-        ))
-    x = 6
-    return x
